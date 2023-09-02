d234a1761fbe549364ea236d12628dc2b12b86c1
puci2020 <adam.pucicki97@gmail.com>
2023-09-02 20:07:34 +0200

    Create translation

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,10 @@
-// module.exports = ({ env }) => ({
-//   email: {
-//     provider: "sendgrid",
-//     providerOptions: {
-//       apiKey: env("SENDGRID_API_KEY"),
-//     },
-//     settings: {
-//       defaultFrom: "myemail@protonmail.com",
-//       defaultReplyTo: "myemail@protonmail.com",
-//     },
-//   },
-// });
 module.exports = ({ env }) => ({
   email: {
     config: {
       provider: "strapi-provider-email-smtp",
       providerOptions: {
         host: process.env.SMTP, //SMTP Host
-        port: 465, //SMTP Port
+        port: process.env.SMTP_PORT, //SMTP Port
         secure: true,
         username: process.env.EMAIL_ADRESS,
         password: process.env.EMAIL_PASSWORD,
---
+++ src/admin/app.js
@@ -0,0 +1,16 @@
+const config = {
+  locales: ["pl"],
+  translations: {
+    pl: {
+      "app.components.HomePage.welcome": "Witaj!",
+      "app.components.HomePage.welcome.again": "Witaj ponownie!",
+    },
+  },
+};
+
+const bootstrap = (app) => {};
+
+export default {
+  config,
+  bootstrap,
+};
